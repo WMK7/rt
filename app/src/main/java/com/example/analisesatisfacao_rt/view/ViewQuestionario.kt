25be88850085b14662ee2e8f472e55ef309f5e7d
Michael <micaelmm36@gmail.com>
2024-09-05 00:01:58 -0400

    implementar no bd a tangibilidade

--- app/src/main/java/com/example/analisesatisfacao_rt/view/ViewQuestionario.kt
+++ app/src/main/java/com/example/analisesatisfacao_rt/view/ViewQuestionario.kt
@@ -7,3 +7,15 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.analisesatisfacao_rt.R
 
+class ViewQuestionario : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_view_adm)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
